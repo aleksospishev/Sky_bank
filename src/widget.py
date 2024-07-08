766fba90abe71dc9ea7c3ff12f727dd69d095686
aleksandr <aleksospishev@yandex.ru>
2024-07-09 00:17:25 +0300

    add file widget, with function mask_account_card

---
+++ src/widget.py
@@ -0,0 +1,17 @@
+from .masks import get_mask_account, get_mask_card_number
+# import masks
+
+TYPE_CARDS = ["visa", "mastercard", "maestro"]
+
+
+def mask_account_card(input_string: str) -> str:
+    """Принимает строку в которой указан номер и тип карты или номер счета,
+    возвращает замаскированную строку"""
+    list_input = input_string.split()
+    if list_input[0].lower() in TYPE_CARDS:
+
+        return f"{' '.join(list_input[:-1])} {get_mask_card_number(list_input[-1])}"
+    elif list_input[0].lower() == "счет":
+        return f"{' '.join(list_input[:-1])} {get_mask_account(list_input[-1])}"
+    else:
+        return "Введены некоректные данные"
