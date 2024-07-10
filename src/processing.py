9e96006a87ad996e4d66451ddcaf1fb005215064
aleksandr <aleksospishev@yandex.ru>
2024-07-10 12:58:54 +0300

    add readme, correct name result,

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,14 @@
 def filter_by_state(list_dictionary: list[dict], state_meaning: str = "EXECUTED") -> list[dict]:
     """Принимает список словарей и опционально значение для ключа state, возвращает новый список словарей,
     содержащий только те словари, у которых ключ state соответствует указанному значению."""
-    resoult = []
+    result = []
     for elem in list_dictionary:
         if state_meaning == elem.get("state"):
-            resoult.append(elem)
-    return resoult
+            result.append(elem)
+    return result
 
 
-def sort_by_date(list_dictionary: list[dict], ascending: bool = True) -> list[dict]:
+def sort_by_date(list_dictionary: list[dict], ascending: bool = False) -> list[dict]:
     """принимает список словарей и необязательный параметр, задающий порядок сортировки, возвращать новый список,
     отсортированный по дате."""
-    return sorted(list_dictionary, key=lambda list_dictionary: list_dictionary["date"], reverse=ascending)
+    return sorted(list_dictionary, key=lambda list_dictionary: list_dictionary["date"], reverse=not ascending)
