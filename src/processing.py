0fdaa0cbaf23330e9a7516f55851e9dea1e770be
aleksandr <aleksospishev@yandex.ru>
2024-07-13 00:01:11 +0300

    add test_func in: test_masks.py, test_processing.py, test_widget.py

--- src/processing.py
+++ src/processing.py
@@ -11,5 +11,4 @@ def filter_by_state(list_dictionary: list, state_meaning: str = "EXECUTED") -> l
 def sort_by_date(list_dictionary: list, descending: bool = True) -> list:
     """принимает список словарей и необязательный параметр, задающий порядок сортировки, возвращать новый список,
     отсортированный по дате."""
-
     return sorted(list_dictionary, key=lambda list_dictionary: list_dictionary.get("date"), reverse=descending)
